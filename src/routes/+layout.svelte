f85ca87bbc0643ddb063aae566ba569dacc7acf6
itsmu1x <itsmu1x@gmail.com>
2024-04-05 21:49:29 +0300

    add: meta tags for seo, theme-color for embedded

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,11 @@
     import "../app.css"
 </script>
 
+<svelte:head>
+    <title>PGxPO</title>
+    <meta name="keywords" content="pgxpo, minecraft, pvp, server, gaming" />
+    <meta name="description" content="PGxPO Minecraft Server" />
+    <meta name="theme-color" content="#4c246e" />
+</svelte:head>
+
 <slot />
